a44cba72062151cd7e414f681e5c2076551a8788
ted050706 <ted050706>
2026-07-10 20:09:08 +0800

    upload

---
+++ src/main/java/com/example/demo/rental/model/dto/auth/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.example.demo.rental.model.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponse {
+	
+	private String token;
+	private String tokenType;
+	private UserProfileDto user;
+	
+}
---
+++ src/main/java/com/example/demo/rental/repository/ReservationRepository.java
@@ -0,0 +1,44 @@
+package com.example.demo.rental.repository;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.rental.model.entity.Reservation;
+import com.example.demo.rental.model.enums.ReservationStatus;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+	List<Reservation> findByUserIdOrderByCreatedAtDesc(Long userId);
+	List<Reservation> findAllByOrderByCreatedAtDesc();
+	
+	@Query("""
+			SELECT COUNT(r) > 0 FROM Reservation r
+			WHERE r.id = :itemId
+				AND r.status IN :statuses
+				AND r.startTime < :newEndDate
+				AND r.endTime > :newStartDate
+			""")
+	boolean exitsOverlap(
+			@Param("itemId") Long itemId,
+			@Param("statuses") Collection<ReservationStatus> statuses,
+			@Param("newEndDate") LocalDateTime newEndDate,
+			@Param("newStartDate") LocalDateTime newStartDate
+			);
+	
+/*	
+ // ItemId And Status In  StartTime LessThan And EndTime GreaterThan
+	boolean existsByItemIdAndStatusInStartTimeLessThanAndEndTimeGreaterThan(
+			Long itemId,
+			Collection<ReservationStatus> statuses,
+			LocalDateTime newEndDate,
+			LocalDateTime newStartDate
+			);
+*/
+	
+}
